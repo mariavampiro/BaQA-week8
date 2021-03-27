468521536e9e3a11154828ae25475be683af56c1
mariavampiro <72162944+mariavampiro@users.noreply.github.com>
2021-03-27 19:42:22 -0300

    added exercise 1

---
+++ ejercicio1.js
@@ -0,0 +1,22 @@
+// Variables y operadores:
+
+//a
+
+var a = 2;
+var b = 3;
+var suma = a + b;
+console.log('La suma es ' +suma);
+
+//b
+
+var st1='hello';
+var st2='world';
+var st3=st1+st2;
+console.log(st3);
+
+//c
+
+var name='maria';
+var surname='vampiro';
+var sum=name.length+surname.length;
+console.log(sum);
